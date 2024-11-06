818cb7f74e60a70d9636d7a5c25fef2f88c67b0d
JorgeRuedaP <jorgeruedaplo2004@gmail.com>
2024-11-06 13:54:22 +0100

    He creado la Funcion EdadCompleta, he modificado el menu y las funciones EdadDias y EdadMes

--- Tema5/src/Ejercicios/Ej27.java
+++ Tema5/src/Ejercicios/Ej27.java
@@ -1,6 +1,7 @@
 package Ejercicios;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
@@ -9,11 +10,10 @@ public class Ej27 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		LocalDate ld=LocalDate.now();
+		LocalDate ld=Funciones26.dimeFecha("Escribe una fecha en este formato: ", sc);
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		int n=0;
 		do {
-			ld=Funciones26.dimeFecha("Escribe una fecha en este formato: ", sc);
 			System.out.println("Menu:");
 			System.out.println("1. Calcular edad en dias");
 			System.out.println("2. Calcular edad en meses");
@@ -24,14 +24,17 @@ public class Ej27 {
 			switch (n) {
 			case 1:
 				int Menu1=EdadDias(ld,sc);
-				System.out.println(Menu1+" dias para tu cumpleaños");
+				System.out.println(Menu1+" dias desde tu nacimiento");
+				System.out.println("");
 				break;
 			case 2:
 				int Menu2=EdadMes(ld,sc);
-				System.out.println(Menu2+" meses para tu cumpleaños");
+				System.out.println(Menu2+" meses desde tu nacimiento");
+				System.out.println("");
 				break;
 			case 3:
-				
+				EdadCompleta(ld,sc);
+				System.out.println("");
 				break;
 			case 4:
 				
@@ -39,18 +42,26 @@ public class Ej27 {
 			case 0:
 				break;
 			}
-			break;
-		} while (n==0);
+		} while (n>0);
 	}
 	static public int EdadDias(LocalDate ld,Scanner sc) {
 		LocalDate hoy=LocalDate.now();
-		int dias = (int) ChronoUnit.DAYS.between(hoy,ld);
+		int dias = (int) ChronoUnit.DAYS.between(ld,hoy);
 		return dias;
 	}
 	static public int EdadMes(LocalDate ld,Scanner sc) {
 		LocalDate hoy=LocalDate.now();
-		int mes = (int) ChronoUnit.MONTHS.between(hoy,ld);
+		int mes = (int) ChronoUnit.MONTHS.between(ld,hoy);
 		return mes;
+	}
+	static public void EdadCompleta(LocalDate ld,Scanner sc) {
+		LocalDate hoy=LocalDate.now();
+		/*int anio = (int) ChronoUnit.YEARS.between(ld,hoy);
+		int mes = (int) ChronoUnit.MONTHS.between(ld.plusYears(anio),hoy);
+		int dia = (int) ChronoUnit.DAYS.between(ld.plusMonths(mes).plusYears(anio),hoy);
+		*/
+		Period p = Period.between(ld, hoy);
+		System.out.println(p.getYears()+" años "+p.getMonths()+" meses "+p.getDays()+" dias");
 	}
 
 }
